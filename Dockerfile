e3ceb39838b9f187bd8b78d995b20ada09eae990
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-27 22:44:25 +0200

    Test with vendor map muahahah i want to see the world burn

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM php:7.4-cli
 
-RUN pecl install zip && docker-php-ext-enable zip
-
 COPY ./* ./
 
 CMD [ "php", "./main.php" ]
--- main.php
+++ main.php
@@ -1,19 +1,30 @@
 <?php
 
-$url = 'https://api.github.com/repos/doctrine/instantiator/zipball/f350df0268e904597e3bd9c4685c53e0e333feea';
-$file = 'tmp.zip';
-$path = '/tmp/code';
+require 'vendor/autoload.php';
 
-file_put_contents($file, file_get_contents($url));
+$url = 'https://api.apify.com/v2/key-value-stores/' . $_ENV['APIFY_DEFAULT_KEY_VALUE_STORE_ID'] . '/records/' . $_ENV['APIFY_INPUT_KEY'];
 
-
-$zip = new ZipArchive;
-$res = $zip->open($file);
-if ($res === TRUE) {
-    // extract it to the path we determined above
-    $zip->extractTo($path);
-    $zip->close();
-    var_dump(scandir($path));
-} else {
-    echo "Doh! I couldn't open $file";
-}
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', $url);
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type')[0];
+// 'application/json; charset=utf8'
+echo $res->getBody();
+//$url = 'https://api.github.com/repos/doctrine/instantiator/zipball/f350df0268e904597e3bd9c4685c53e0e333feea';
+//$file = 'tmp.zip';
+//$path = '/tmp/code';
+//
+//file_put_contents($file, file_get_contents($url));
+//
+//
+//$zip = new ZipArchive;
+//$res = $zip->open($file);
+//if ($res === TRUE) {
+//    // extract it to the path we determined above
+//    $zip->extractTo($path);
+//    $zip->close();
+//    var_dump(scandir($path));
+//} else {
+//    echo "Doh! I couldn't open $file";
+//}
